0d822e37c2ad74fb0257fb9bb8da8399e6f94cb0
PawanPawar11 <pawanpawar4369@gmail.com>
2026-07-07 07:29:11 +0530

    feat: create a new add-meal page/screen

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -10,7 +10,10 @@ export default function RootLayout() {
       }}
     >
       <Stack.Screen name="(tabs)/index" options={{ headerShown: false }} />
+      {/* The children to be rendered on initial render is the one who sits at the top of the stack i.e., (tabs)/index in our case */}
       <Stack.Screen options={{ title: "Meals" }} name="meals" />
+      <Stack.Screen options={{ title: "Add Meal" }} name="add-meal" />
+      {/* Here, you can only render the children which are present inside the App Folder as far I know */}
     </Stack>
   );
 }
